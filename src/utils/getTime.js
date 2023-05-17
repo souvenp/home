c2f9be455bda4fc8e9f15c1d46e47c4d6e32d003
harenchi <64030750+souvenp@users.noreply.github.com>
2023-05-17 17:31:54 +0800

    Update getTime.js

--- src/utils/getTime.js
+++ src/utils/getTime.js
@@ -105,7 +105,7 @@ export const helloInit = () => {
   }
   ElMessage({
     dangerouslyUseHTMLString: true,
-    message: `<strong>${hello}</strong> ようこそ楽園へ`,
+    message: `<strong></strong> ようこそ楽園へ`,
   });
 };
 
